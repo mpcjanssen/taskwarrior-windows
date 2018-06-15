bc0b853173537bc408e9a589b0efd84fcb3e64d1
Mark Janssen <mpc.janssen@gmail.com>
2018-06-15 12:54:43 +0200

    Initial commit

---
+++ cmd/task/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+func main() {
+	newArgs := make([]string, len(os.Args)+1)
+	newArgs[0] = "task"
+	for idx, arg := range os.Args[1:] {
+		newArgs[idx+1] = "'" + arg + "'"
+		// fmt.Printf("%d%s\n", idx+1, newArgs[idx])
+	}
+
+	cmd := exec.Command("bash", "-c", strings.Join(newArgs, " "))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Run()
+
+	status := cmd.ProcessState.Sys().(syscall.WaitStatus)
+	os.Exit(status.ExitStatus())
+
+}
